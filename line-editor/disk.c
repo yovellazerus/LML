aa4d2ad67c90d10c9ff1e86a39af9da1a51b59ef
yovellazerus <129679597+yovellazerus@users.noreply.github.com>
2025-10-27 19:54:41 +0200

    more FAT code

--- line-editor/disk.c
+++ line-editor/disk.c
@@ -1,104 +1,120 @@
 
 #include <stdio.h>
-#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define SECTOR_SIZE 256
 #define NUMBER_OF_SECTORS (8*256)
+#define FAT_SIZE 16
+#define DIR_PER_SEC 8
+#define ROOT_DIR_NUMBER FAT_SIZE
 
-typedef uint8_t byte;
+typedef unsigned char byte;
 
-typedef struct Sector {
-    byte data[SECTOR_SIZE];
-} Sector;
+typedef struct Dir {
+    byte name[16];
+    byte attr;
+    byte size[3];
+    byte fat_entry[2];
+    byte time_stamp[4];
+    byte reserve[6];
+} Dir;
 
-typedef struct Disk {
-    Sector data[NUMBER_OF_SECTORS];
-} Disk;
-
-void read_sector(Disk* disk, int sector_number, Sector* buffer){
-    if(!disk || sector_number < 0 || sector_number >= NUMBER_OF_SECTORS || !buffer) return;
-    memcpy(&buffer->data, &disk->data[sector_number].data, SECTOR_SIZE);
-} 
-
-void write_sector(Disk* disk, int sector_number, Sector* buffer){
-    if(!disk || sector_number < 0 || sector_number >= NUMBER_OF_SECTORS || !buffer) return;
-    memcpy(&disk->data[sector_number].data, buffer->data, SECTOR_SIZE);
+void read_secotor(byte disk[NUMBER_OF_SECTORS][SECTOR_SIZE], byte buffer[SECTOR_SIZE], int sector_number){
+    if(!disk || !buffer || sector_number < 0 || sector_number >= NUMBER_OF_SECTORS) return;
+    memcpy(buffer, disk[sector_number], SECTOR_SIZE);
 }
 
-void print_sector(Sector* buffer, int sector_number){
-    if(!buffer) return;
-    printf("sector: %d", sector_number);
+void write_secotor(byte disk[NUMBER_OF_SECTORS][SECTOR_SIZE], byte buffer[SECTOR_SIZE], int sector_number){
+    if(!disk || !buffer || sector_number < 0 || sector_number >= NUMBER_OF_SECTORS) return;
+    memcpy(disk[sector_number], buffer, SECTOR_SIZE);
+}
+
+void write_dir(byte disk[NUMBER_OF_SECTORS][SECTOR_SIZE], int sector_number, Dir* dir){
+    if(!disk || !dir || sector_number < 0 || sector_number >= NUMBER_OF_SECTORS) return;
+    
+    byte buffer[SECTOR_SIZE] = {0};
+    read_secotor(disk, buffer, sector_number);
+    int i;
+    for(i = 0; i < DIR_PER_SEC; i++){
+        if(buffer[i*32] == '\0'){
+            break;
+        }
+    }
+    if(i == DIR_PER_SEC){
+        printf("sector full.\n");
+        return;
+    }
+    
+    memcpy(buffer + i*32 + 0, dir->name, 16);
+    memcpy(buffer + i*32 + 16, &dir->attr, 1);
+    memcpy(buffer + i*32 + 17, dir->size, 3);
+    memcpy(buffer + i*32 + 20, dir->fat_entry, 2);
+    memcpy(buffer + i*32 + 22, dir->time_stamp, 4);
+    memcpy(buffer + i*32 + 26, dir->reserve, 6);
+    
+    write_secotor(disk, buffer, sector_number);
+}
+
+void write_to_fat(byte disk[NUMBER_OF_SECTORS][SECTOR_SIZE], int entry, int next_claster){
+   
+}
+
+void dump_sector(byte disk[NUMBER_OF_SECTORS][SECTOR_SIZE], int sector_number){
+    if(!disk || sector_number < 0 || sector_number >= NUMBER_OF_SECTORS) return;
+    printf("sector number: %d", sector_number);
     for(int i = 0; i < SECTOR_SIZE; i++){
         if(i % 16 == 0){
             printf("\n%.2X: ", i);
         }
-        printf("%.2X ", buffer->data[i]);
+        printf("%.2X ", disk[sector_number][i]);
     }
     printf("\n");
-} 
+}
+
+void dump_fat(byte disk[NUMBER_OF_SECTORS][SECTOR_SIZE]){
+    if(!disk) return;
+    printf("FAT:\n" "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    for(int i = 0; i < FAT_SIZE; i++){
+        dump_sector(disk, i);
+    }
+}
 
 int main()
 {
+    byte disk[NUMBER_OF_SECTORS][SECTOR_SIZE] = {0};
+    
     byte buffer[SECTOR_SIZE] = {0};
-    Disk disk = {0};
-
-    int sector_number = 32;  // reserve for the root dir 
-    Sector s0 = {0};
-    s0.data[0] = 'b';    // name
-    s0.data[1] = 'i';
-    s0.data[2] = 'n';
-
-    s0.data[16] = 0x01;  // type: dir
-
-    s0.data[17] = 0x40;  //size in bytes
-    s0.data[18] = 0x60;
-    s0.data[19] = 0x01;
-
-    s0.data[20] = 0x03;  // fat addr low than hight
-    s0.data[21] = 0x00;
-
-    s0.data[22] = 0x01;  // time stamp
-    s0.data[23] = 0x02;
-    s0.data[24] = 0x03;
-    s0.data[25] = 0x04;
-
-    int bin_number = 3;
-    Sector s1 = {0};
-    s1.data[0] = 'a';    // name
-    s1.data[1] = 's';
-    s1.data[2] = 's';
-    s1.data[3] = 'e';
-    s1.data[4] = 'm';
-    s1.data[5] = 'b';
-    s1.data[6] = 'l';
-    s1.data[7] = 'e';
-    s1.data[8] = 'r';
-    s1.data[9] = '.';
-    s1.data[10] = 'e';
-    s1.data[11] = 'x';
-    s1.data[12] = 'e';
-
-    s1.data[16] = 2;     // type: bin file
-
-    s1.data[17] = 0x00;  //size in bytes
-    s1.data[18] = 0x20;
-    s1.data[19] = 0x00;
-
-    s1.data[20] = 0x05;  // fat addr low than hight
-    s1.data[21] = 0x00;
-
-    s1.data[22] = 0x10;  // time stamp
-    s1.data[23] = 0x20;
-    s1.data[24] = 0x30;
-    s1.data[25] = 0x40;
-
-    write_sector(&disk, sector_number, &s0);
-    write_sector(&disk, bin_number, &s1);
-
-    print_sector(&disk.data[sector_number], sector_number);
-    print_sector(&disk.data[bin_number], bin_number);
+    
+    int bin_num  = 0x0020;
+    int user_num = 0x0070;
+    
+    Dir bin = { .name = {'b', 'i', 'n'}, 
+                .attr = 0x01, 
+                .size = {0x20, 0x30, 0x01}, 
+                .fat_entry = {0x20, 0x00}, 
+                .time_stamp = {0x10, 0x20, 0x30, 0x40}
+        
+    }; 
+    
+    Dir usr = { .name = {'u', 's', 'r'}, 
+                .attr = 0x01, 
+                .size = {0xff, 0xfe, 0x03}, 
+                .fat_entry = {0x70, 0x00}, 
+                .time_stamp = {0x11, 0x21, 0x31, 0x41}
+        
+    }; 
+    
+    write_dir(disk, ROOT_DIR_NUMBER, &bin);
+    write_dir(disk, ROOT_DIR_NUMBER, &usr);
+    
+    printf("root directory: \n" "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    dump_sector(disk, ROOT_DIR_NUMBER);
+    
+    dump_fat(disk);
 
     return 0;
 }
+
+
+
